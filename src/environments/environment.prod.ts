90c3995fc74d1e34425ddfd1fc275264de00b9a9
elpoloxrodriguez <elpoloxrodriguez@gmail.com>
2024-02-26 18:03:00 -0400

    generales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   recaptcha: {
-    siteKey: '6LdwJNwlAAAAAI8-p7XKKCtfJ51goRFyBBbjaAJL',
+    siteKey: '6LdcDsknAAAAAC44xkc214BZ2giOxN8JQsL7L9x2',
   },
   hmr: false,
   apiUrl: 'https://localhost',
   Url: 'https://localhost',
   API: '/v1/api/',
-  ID : 'ID-001',
+  ID: 'ID-001',
   Hash: ':c521f27fb1b3311d686d511b668e5bd4',
   version: 'Enterprise 1.0.1-ecac7af',
   fecha: '2023-03-21 22:08:00'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   recaptcha: {
-    siteKey: '6LdwJNwlAAAAAI8-p7XKKCtfJ51goRFyBBbjaAJL',
+    siteKey: '6LdcDsknAAAAAC44xkc214BZ2giOxN8JQsL7L9x2',
   },
   hmr: false,
   apiUrl: 'https://localhost',
   Url: 'https://localhost', // requerido para la Fnx_Eliminar proyectos 
   API: '/v1/api/',
-  ID : 'ID-001',
+  ID: 'ID-001',
   Hash: ':c521f27fb1b3311d686d511b668e5bd4',
   version: 'Development 1.0.1-ecac7af',
   fecha: '2023-03-21 22:08:00'
