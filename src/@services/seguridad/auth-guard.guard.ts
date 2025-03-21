b94c043abec3c26fda47c2c57bed1f7e28907b64
Carlos Peña <gesaodin@gmail.com>
2025-03-21 19:34:18 -0400

    Control de datos

--- src/@services/seguridad/auth-guard.guard.ts
+++ src/@services/seguridad/auth-guard.guard.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from './login.service';
-import { environment } from 'environments/environment';
 import jwt_decode from "jwt-decode";
+
+import {Md5} from 'ts-md5';
+import { UtilService } from '@services/util/util.service';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +15,7 @@ export class AuthGuardGuard implements CanActivate {
 
   public token
 
-  constructor(private router: Router, private loginService: LoginService) { }
+  constructor(private router: Router,  private util: UtilService, private loginService: LoginService) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -23,12 +26,25 @@ export class AuthGuardGuard implements CanActivate {
     const IDAPP = this.token.Usuario.Aplicacion[0].id
 
     if (sessionStorage.getItem("token") !== undefined && IDAPP == 'ID-001') {
-      return true;
+      let menu = JSON.parse(sessionStorage.getItem('menu')) 
+      let texto = Md5.hashStr(sessionStorage.getItem('menu'))
+      let validar = sessionStorage.getItem('crypt')
+      if(texto == validar ){
+        let valor = false
+        if (state.url == '/home') return true
+        menu.forEach(e => {
+          e.SubMenu.forEach(xe => {
+             if (state.url.indexOf(xe.url) == 1 ) return valor=true
+          });
+        })
+        if(!valor)this.msj()
+        return valor
+      }else{
+        return false
+      }
+     
     } else {
-      console.log('Entrando');
-      sessionStorage.removeItem("token");
-      sessionStorage.removeItem("id");
-      this.router.navigate(['login']);
+      this.router.navigate(['/home']);
       return false;
     }
 
@@ -39,13 +55,21 @@ export class AuthGuardGuard implements CanActivate {
 
   authConecting(): Promise<boolean> {
     return new Promise<boolean>((resolv, reject) => {
-      // firebase.auth().onAuthStateChanged( user => {
-      //   if(user){
-      //     return resolv(true);
-      //   }else{
-      //     return reject(false);
-      //   }
-      // })
+ 
+    })
+  }
+
+  msj(){
+    Swal.fire({
+      title: 'Área restringida',
+      text: 'No poseé autorización',
+      icon: 'error',
+      showCancelButton: false,
+      confirmButtonColor: '#3085d6',
+      confirmButtonText: 'Ok',
+      allowEscapeKey: true,
+    }).then((result) => {
+      //
     })
   }
 }
