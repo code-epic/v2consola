2eb0ad4bc65c747eb7b74aa0a7017a35b20fe5c2
elpoloxrodriguez <elpoloxrodriguez@gmail.com>
2025-04-10 18:32:38 -0400

    Lista de Usuarios

--- src/app/main/views/security/user-list/user-list.component.ts
+++ src/app/main/views/security/user-list/user-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { ColumnMode, DatatableComponent, SelectionType, } from "@swimlane/ngx-datatable";
+import { ApiService, IAPICore } from '@services/apicore/api.service';
 
 @Component({
   selector: 'app-user-list',
@@ -16,19 +17,52 @@ export class UserListComponent implements OnInit {
   @BlockUI() blockUI: NgBlockUI;
   @BlockUI('section-block') sectionBlockUI: NgBlockUI;
 
+
+  public xAPI: IAPICore = {
+    funcion: '',
+    parametros: '',
+    relacional: false,
+    concurrencia: false,
+    protocolo: '',
+    ruta: '',
+    retorna: false,
+    migrar: false,
+    modulo: '',
+    valores: {},
+    coleccion: '',
+    http: 0,
+    https: 0,
+    consumidores: 0,
+    puertohttp: 0,
+    puertohttps: 0,
+    driver: '',
+    query: '',
+    metodo: '',
+    tipo: '',
+    prioridad: '',
+    entorno: '',
+    logs: false
+  };
+
   public basicSelectedOption: number = 10;
   public ColumnMode = ColumnMode;
   public SelectionType = SelectionType;
 
 
   public contentHeader: object;
-  public tempData = [];
-  public rowData = [];
+  public count: number = 0;
+  public tempData = []
+  public rowData = []
 
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService
+  ) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+
+    await this.listarUsuarios();
+
     this.contentHeader = {
       headerTitle: "Aplicaciones",
       actionButton: true,
@@ -51,6 +85,40 @@ export class UserListComponent implements OnInit {
         ],
       },
     };
+  }
+
+  async listarUsuarios() {
+    this.xAPI.funcion = "_SYS_LstUsuarios";
+    this.xAPI.parametros = ''
+    await this.apiService.Ejecutar(this.xAPI).subscribe(
+      (data) => {
+        this.rowData = data;
+        this.tempData = this.rowData;
+        this.count = this.rowData.length
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  }
+
+  filterUpdate(event: any) {
+    const val = event.target.value ? event.target.value.toLowerCase() : '';
+    // filter our data
+    const temp = this.tempData.filter(function (d) {
+      return d.login.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+    // update the rows
+    this.rowData = temp;
+    this.count = this.rowData.length;
+    // Whenever the filter changes, always go back to the first page
+    if (this.table) {
+      this.table.offset = 0;
+    }
+  }
+
+  EliminarUser(event: any){
+    console.log(event)
   }
 
 }
